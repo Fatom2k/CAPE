03dafe334c0f25699b5a9ff68eb3853753ef8ee3
Fatom2k <54989176+Fatom2k@users.noreply.github.com>
2023-01-16 16:51:44 +0100

    Update preinstall.sh

--- preinstall.sh
+++ preinstall.sh
@@ -6,3 +6,11 @@ chmod +x ./kvm-qemu.sh
 sudo ./cape2.sh base | tee cape2.log
 
 sudo ./kvm-qemu.sh all user | tee kvm-qemu.log
+
+cd /opt/CAPEv2
+
+sudo poetry install
+
+sudo -u cape poetry run extra/poetry_libvirt_installer.sh
+
+
